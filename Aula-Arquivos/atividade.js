acaa4c77c926060af2a73936fa109ed8f3a50a97
AntonioFreire203 <freireantonio203@gmail.com>
2024-10-18 19:46:19 -0300

    update arquitetura readme.md

--- Aula-Arquivos/atividade.js
+++ Aula-Arquivos/atividade.js
@@ -4,9 +4,6 @@
 */
 
 /*1-Escreva uma função que recebe uma mensagem e o nome do arquivo por parâmetros e salva a mensagem em um arquivo TXT.
-
-*/
-
 const fs = require("fs").promises;
 
 async function alterarMensagem(mensagem) {
@@ -22,13 +19,20 @@ try {
 }
 
 alterarMensagem("01234567899876543210");
+*/
+
 
 
 /*
 2-Escreva uma função que faz a cópia de um arquivo adicionando a extensão ".bkp" no nome do mesmo. Se o novo arquivo já existe, uma mensagem de erro deve ser informado na tela. O caminho do arquivo original é informado por parâmetro.
 */
+const fs = require("fs").promises;
+
+
+
+
 
 /*
-Escreva uma função que lê o conteúdo de um arquivo de texto e retorne  a quantidade de linhas que o arquivo possui. O nome do arquivo é informado por parâmetro.
+3-Escreva uma função que lê o conteúdo de um arquivo de texto e retorne  a quantidade de linhas que o arquivo possui. O nome do arquivo é informado por parâmetro.
 
 */

1b714d1e828a3511533d042cc9e2a35c4b1c8860
AntonioFreire203 <freireantonio203@gmail.com>
2024-10-20 22:17:11 -0300

    upgrade atividade 02 de arquivos

--- Aula-Arquivos/atividade.js
+++ Aula-Arquivos/atividade.js
@@ -25,8 +25,36 @@ alterarMensagem("01234567899876543210");
 
 /*
 2-Escreva uma função que faz a cópia de um arquivo adicionando a extensão ".bkp" no nome do mesmo. Se o novo arquivo já existe, uma mensagem de erro deve ser informado na tela. O caminho do arquivo original é informado por parâmetro.
-*/
 const fs = require("fs").promises;
+const path = require("path");
+
+async function criarBackup(caminho) {
+    try {
+        const caminhoBackup = caminho + ".bkp";
+
+        
+        try {
+            await fs.access(caminhoBackup);
+            console.log("Erro: O arquivo de backup já existe.");
+            return;
+        } catch (error) {
+           
+        }
+
+        
+        await fs.copyFile(caminho, caminhoBackup);
+        console.log("Arquivo copiado com sucesso para:", caminhoBackup);
+    } catch (error) {
+        console.log("Erro ao copiar o arquivo:", error.message);
+    }
+}
+criarBackup("Aula-Arquivos\teste.txt");
+
+*/
+
+
+
+
 
 
 
